e179f3db4daf3148ccc8ab4b063b49398bb091ab
UsernameAK <usernameak@neonet.cf>
2016-04-16 12:55:22 +0400

    ap login form

--- admin/index.php
+++ admin/index.php
@@ -1,9 +1,16 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Admin panel - Flame CMS</title>
+	<title>Login - Admin panel - Flame CMS</title>
 </head>
 <body>
-	
+	<h1>Admin panel - Restricted Area</h1>
+	<form method="POST" action="/admin/check.php">
+		<div>Login: <input type="text" name="login"></div>
+		<div>Password: <input type="password" name="pass"></div>
+		<div><input type="submit" value="Login"></div>
+	</form>
+	<hr>
+	<p><em>Flame CMS v0.1a</em></p>
 </body>
 </html>
