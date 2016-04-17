cde8b71e0d38740ca46b964028c232baa892dc2e
UsernameAK <usernameak@neonet.cf>
2016-04-17 14:23:25 +0400

    ссылка на выход

--- admin/panel.php
+++ admin/panel.php
@@ -1,5 +1,13 @@
 <?php
 	require_once($_SERVER["DOCUMENT_ROOT"]."/admin/check.php");
 	check_cookie(true);
-	echo "Goodbye, world!";
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Admin panel - Flame CMS</title>
+</head>
+<body>
+<a href="/admin/check.php?quit=1">Logout</a>
+</body>
+</html>
