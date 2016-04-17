eb99fa3ebdb2c58a7874c38783a7e3d2d83c3c3e
FlameNeon <flameneon.m@gmail.com>
2016-04-17 18:51:27 +1100

    bread

--- modules/news.php
+++ modules/news.php
@@ -1,4 +1,4 @@
-<?
+﻿<?
 	function index_page()
 	{
 		include("config.php");
@@ -9,16 +9,43 @@
 			$file_f = file("resourses/news/".$i.".txt");
 			$title[$i] = $file_f[0];
 			$date_b[$i] = $file_f[1];
-			$tags[$i] = $file_f[2];
-			$cats[$i] = $file_f[3];
+			$desc[$i] = $file_f[2];
 			
-			for($j = 4; $j < count($file_f); $j ++)
+			
+			for($j = 6; $j < count($file_f); $j ++)
 			{
-				$txt[$i] .= "\n $file_f[$j] ";	
+				$txt[$i] .= $file_f[$j];	
 			}
 			
 
 			$txt[$i] = bbcodd($txt[$i]);
+			
+			$cat_t = file_get_contents("templates/$template/breadcrumbs.html");
+			$catm = str_replace("[_cat]", "Главная", $cat_t);
+			$cat_1 = str_replace("[_cat]", $file_f[3], $cat_t);
+			$sub = $file_f[4];
+			$subs = $file_f[5];
+			
+			
+			if(preg_match("nul", $sub))
+			{
+				$cat_2 = str_replace("[_cat]", $sub, $cat_t);
+			}
+			else
+			{
+				$cat_2 = str_replace("[_cat]", "", $cat_t);
+			}
+			
+			if(preg_match("nul", $subs))
+			{
+				$cat_3 = str_replace("[_cat]", $subs, $cat_t);
+			}
+			else
+			{
+				$cat_3 = str_replace("[_cat]", "", $cat_t);
+			}
+			
+			$cats[$i] = $catm.$cat_1.$cat_2.$cat_3;
 			
 			$author[$i] = "Neongames";
 			
@@ -26,7 +53,7 @@
 			$edd_tamp = str_replace("[_text]", $txt[$i], $edd_tamp);
 			$edd_tamp = str_replace("[_title]", $title[$i], $edd_tamp);
 			$edd_tamp = str_replace("[_cat]", $cats[$i], $edd_tamp);
-			$edd_tamp = str_replace("[_desc]", $tags[$i], $edd_tamp);
+			$edd_tamp = str_replace("[_desc]", $desc[$i], $edd_tamp);
 			$edd_tamp = str_replace("[_author]", $author[$i], $edd_tamp);
 			$edd_tamp = str_replace("[_date_b]", $date_b[$i], $edd_tamp);
 
