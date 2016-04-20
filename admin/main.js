a3fba16e25b8902ae6a96a747d448a385f6313d6
UsernameAK <usernameak@neonet.cf>
2016-04-20 13:50:28 +0400

    cp

---
+++ admin/main.js
@@ -0,0 +1,7 @@
+$('.ap-menuitem').on("click", function() {
+	$.ajax($(this).data("panefile")).done(function(data) {
+		$("main").removeClass("indeterminate-main error-main").html(data);
+	}).fail(function() {
+		$("main").removeClass("indeterminate-main").addClass("error-main").html("Error loading pane");
+	})
+})
---
+++ admin/panes/settings.php
@@ -0,0 +1,4 @@
+<div id="settings-list">
+<div id="settings-item"><label>Имя сайта: </label><input></div>
+<div id="settings-item"><label>Шаблон сайта: </label><input></div>
+</div>
