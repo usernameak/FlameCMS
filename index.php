a747d9cfb4b97e281470bde2d8af0fcf49bf5217
FlameNeon <flameneon.m@gmail.com>
2016-05-03 17:25:35 +1100

    Слил вместе news.php и blog.php.

--- index.php
+++ index.php
@@ -3,42 +3,18 @@
 	
 	if($_GET['blog'])$blog = $_GET['blog'];
 	
+	include("modules/blog.php");
 	if(!$blog)
 	{
-		include("modules/news.php");
 		$news = index_page();
-	}
-	
-	if($blog)
+	}elseif($blog)
 	{
-		include("modules/blog.php");
-		$txt = blog($blog);
+		$news = blog($blog);
 	}
 	
 	include("modules/menu.php");
 	$menu = menu();
 	
-	if(!empty($_GET['page']))
-	{
-		$page_content = $gallery;
-		include("templates/$template/main.html");
-	}
-	
-	if(!empty($_GET['tpage']))
-	{
-		include("modules/pages.php");
-		
-		$page_id = $_GET['tpage'];
-		$page = gettpage($page_id);
-		$page_title = $page[0];
-		$page_content = $page[1];
-		
-		include("templates/$template/main.html");
-	}
-	else
-	{
-		$page_title;
-		$page_content = $news;
-		include("templates/$template/main.html");
-	}
+	$page_content = $news;
+	include("templates/$template/main.html");
 ?>
